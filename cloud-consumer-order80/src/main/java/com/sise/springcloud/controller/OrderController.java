0e663dda7a835c101f1cd05ca4b66645eed3d0b8
myth-git <1610680426@qq.com>
2022-04-01 19:06:14 +0800

    客服端入驻7001eureka

--- cloud-consumer-order80/src/main/java/com/sise/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/sise/springcloud/controller/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.net.URL;
+
 
 @RestController
 @Slf4j

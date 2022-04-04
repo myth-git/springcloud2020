ce2bc1a066a1e4541fd3e80251446e30b617694a
myth-git <1610680426@qq.com>
2022-04-04 18:37:50 +0800

    OpenFeign超时控制

--- cloud-provider-payment8001/src/main/java/com/sise/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/sise/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -56,6 +57,16 @@ public class PaymentController {
             log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try { TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace(); }
+        return serverPort;
     }
 
 

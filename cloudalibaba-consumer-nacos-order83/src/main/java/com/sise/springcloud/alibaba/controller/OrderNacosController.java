4d18c9ce6bd0155124fa14963fd30630ddd8263d
myth-git <1610680426@qq.com>
2022-04-06 09:57:43 +0800

    搭建nacos消费者83

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/sise/springcloud/alibaba/controller/OrderNacosController.java
@@ -0,0 +1,26 @@
+package com.sise.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderNacosController {
+
+    @Resource
+    private RestTemplate restTemplate;
+    //采用获取yaml配置文件获取提供者的微服务名称
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+}

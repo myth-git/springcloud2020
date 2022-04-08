45c312473ea626bf97bda8dd6122412f28bd930e
myth-git <1610680426@qq.com>
2022-04-08 18:51:49 +0800

    搭建基础环境84消费者9003、9004提供者修改

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/sise/springcloud/alibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/sise/springcloud/alibaba/controller/CircleBreakerController.java
@@ -23,8 +23,7 @@ public class CircleBreakerController {
     //@SentinelResource(value = "fallback") //没有配置
     //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
     //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
-    @SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler",
-            exceptionsToIgnore = {IllegalArgumentException.class})
+    @SentinelResource(value = "fallback")
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
 

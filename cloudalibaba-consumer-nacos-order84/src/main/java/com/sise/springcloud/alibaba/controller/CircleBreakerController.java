16f486286c46e22a7e69a551996de1bf3074675b
myth-git <1610680426@qq.com>
2022-04-08 19:21:36 +0800

    熔断配置

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/sise/springcloud/alibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/sise/springcloud/alibaba/controller/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package com.sise.springcloud.alibaba.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.sise.springcloud.entities.CommonResult;
 import com.sise.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,8 @@ public class CircleBreakerController {
     //@SentinelResource(value = "fallback") //没有配置
     //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
     //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
-    @SentinelResource(value = "fallback")
+     @SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
+                exceptionsToIgnore = {IllegalArgumentException.class})//忽略配置
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
 
@@ -34,5 +36,16 @@ public class CircleBreakerController {
         }
 
         return result;
+    }
+    //本例是blockHandler
+    public CommonResult blockHandler(@PathVariable  Long id, BlockException blockException) {
+        Payment payment = new Payment(id,"null");
+        return new CommonResult<>(445,"blockHandler-sentinel限流,无此流水: blockException  "+blockException.getMessage(),payment);
+    }
+
+    //本例是fallback
+    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
+        Payment payment = new Payment(id,"null");
+        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
     }
 }
